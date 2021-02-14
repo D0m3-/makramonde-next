b1f6377ee4797516a7566e33d2b8df85e6e99b35
Florian Bernard <ext.florian.bernard@king.com>
2021-02-14 21:13:11 +0100

    fix css

--- next.config.js
+++ next.config.js
@@ -1,44 +1,22 @@
-const withLess = require('@zeit/next-less');
 const lessToJS = require('less-vars-to-js');
 const fs = require('fs');
 const path = require('path');
-const withCss = require('@zeit/next-css');
+const withStyles = require('@webdeb/next-styles');
 
 const themeVariables = lessToJS(
   fs.readFileSync(path.resolve(__dirname, './src/makramonde.less'), 'utf8')
 );
 
-module.exports = withCss({
-  cssModules: true,
-  ...withLess({
-    lessLoaderOptions: {
-      javascriptEnabled: true,
-      modifyVars: themeVariables,
-    },
-    webpack: (config, { isServer, dev }) => {
-      if (isServer) {
-        const antStyles = /(antd\/.*?\/style).*(?<![.]js)$/;
-        const origExternals = [...config.externals];
-        config.externals = [
-          (context, request, callback) => {
-            if (request.match(antStyles)) return callback();
-            if (typeof origExternals[0] === 'function') {
-              origExternals[0](context, request, callback);
-            } else {
-              callback();
-            }
-          },
-          ...(typeof origExternals[0] === 'function' ? [] : origExternals),
-        ];
-
-        config.module.rules.unshift({
-          test: antStyles,
-          use: 'null-loader',
-        });
-      }
-      config.resolve.alias['@'] = path.resolve(__dirname);
-      return config;
-    },
-  }),
+module.exports = withStyles({
+  less: true,
+  modules: true,
+  cssLoaderOptions: {
+    importLoaders: 1,
+    localIdentName: '[local]___[hash:base64:5]',
+  },
+  lessLoaderOptions: {
+    javascriptEnabled: true,
+    modifyVars: themeVariables,
+  },
   target: 'serverless',
 });
